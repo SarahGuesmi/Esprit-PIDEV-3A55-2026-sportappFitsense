1a75cd28bd68179fe181b0bd7eee9237207c3fd5
Nourram <nourammarr9@gmail.com>
2026-02-11 18:10:31 +0100

    feat(questionnaire: add questionnaire entity and related features)

---
+++ src/Entity/Workout.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity()]
+class Workout
+{
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $niveau = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $duree = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(?string $niveau): self
+    {
+        $this->niveau = $niveau;
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?int $duree): self
+    {
+        $this->duree = $duree;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
---
+++ src/Repository/WorkoutRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Workout;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Workout>
+ *
+ * @method Workout|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Workout|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Workout[]    findAll()
+ * @method Workout[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class WorkoutRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Workout::class);
+    }
+}
