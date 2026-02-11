b012a71fd861403209925325c84d4c737691d1d4
Nourram <nourammarr9@gmail.com>
2026-02-11 18:57:29 +0100

    feat(rest)

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Questionnaire;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,5 +14,29 @@ class HomeController extends AbstractController
  public function index(): Response
   {
     return $this->render('home/index.html.twig');
+  }
+
+  #[Route('/dashboard', name: 'dashboard')]
+  public function dashboard(): Response
+  {
+    return $this->render('home/dashboard.html.twig');
+  }
+
+  #[Route('/coach/questionnaires', name: 'coach_questionnaires')]
+  public function coachQuestionnaires(EntityManagerInterface $em, Request $request): Response
+  {
+    $search = $request->query->get('search', '');
+    $repository = $em->getRepository(Questionnaire::class);
+
+    if (!empty($search)) {
+      $questionnaires = $repository->findByUserNameLike($search);
+    } else {
+      $questionnaires = $repository->findAll();
+    }
+
+    return $this->render('coach/questionnaires.html.twig', [
+      'questionnaires' => $questionnaires,
+      'search' => $search,
+    ]);
   }
 }
